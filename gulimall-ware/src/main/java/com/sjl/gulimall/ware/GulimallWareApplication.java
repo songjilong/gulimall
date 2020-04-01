081b94cae7859fe6c226e205ba45f7858c927010
songjilong <2606587750@qq.com>
2020-04-01 18:25:20 +0800

    项目初始结构创建

---
+++ gulimall-ware/src/main/java/com/sjl/gulimall/ware/GulimallWareApplication.java
@@ -0,0 +1,13 @@
+package com.sjl.gulimall.ware;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GulimallWareApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GulimallWareApplication.class, args);
+    }
+
+}
---
+++ gulimall-ware/src/test/java/com/sjl/gulimall/ware/GulimallWareApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sjl.gulimall.ware;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GulimallWareApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
