d6cdeae8abf4d1fe2b8b02d32e230b2fe17e679e
songjilong <2606587750@qq.com>
2020-04-08 23:39:02 +0800

    添加分类属性接口

--- gulimall-product/src/main/java/com/sjl/gulimall/product/controller/AttrGroupController.java
+++ gulimall-product/src/main/java/com/sjl/gulimall/product/controller/AttrGroupController.java
@@ -33,10 +33,12 @@ public class AttrGroupController {
     /**
      * 列表
      */
-    @RequestMapping("/list")
+    @RequestMapping("/list/{catelogId}")
     //@RequiresPermissions("product:attrgroup:list")
-    public R list(@RequestParam Map<String, Object> params){
-        PageUtils page = attrGroupService.queryPage(params);
+    public R list(@RequestParam Map<String, Object> params,
+                  @PathVariable("catelogId") Long catelogId){
+//        PageUtils page = attrGroupService.queryPage(params);
+        PageUtils page = attrGroupService.queryPage(params, catelogId);
 
         return R.ok().put("page", page);
     }
--- gulimall-product/src/main/java/com/sjl/gulimall/product/service/impl/AttrGroupServiceImpl.java
+++ gulimall-product/src/main/java/com/sjl/gulimall/product/service/impl/AttrGroupServiceImpl.java
@@ -1,7 +1,10 @@
 package com.sjl.gulimall.product.service.impl;
 
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.stereotype.Service;
+
 import java.util.Map;
+
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.baomidou.mybatisplus.core.metadata.IPage;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
@@ -23,6 +26,27 @@ public class AttrGroupServiceImpl extends ServiceImpl<AttrGroupDao, AttrGroupEnt
                 new QueryWrapper<AttrGroupEntity>()
         );
 
+        return new PageUtils(page);
+    }
+
+    @Override
+    public PageUtils queryPage(Map<String, Object> params, Long catelogId) {
+        IPage<AttrGroupEntity> page;
+        if (catelogId == 0) {
+            //查询所有
+            page = this.page(new Query<AttrGroupEntity>().getPage(params),
+                    new QueryWrapper<>());
+        } else {
+            QueryWrapper<AttrGroupEntity> wrapper = new QueryWrapper<>();
+            //根据 catelogId 查询
+            wrapper.eq("catelog_id", catelogId);
+            //如果key不为空，再加上 key 的查询条件
+            String key = (String) params.get("key");
+            if (StringUtils.isNotBlank(key)) {
+                wrapper.and(obj -> obj.eq("attr_group_id", key).or().like("attr_group_name", key));
+            }
+            page = this.page(new Query<AttrGroupEntity>().getPage(params), wrapper);
+        }
         return new PageUtils(page);
     }
 
